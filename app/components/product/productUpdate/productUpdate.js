c99521cc36539375a49c750d55371a8355b57842
andresarangoo <arango.ortiz.felipe@accenture.com>
2021-07-22 08:58:35 -0500

    Update update with product amount

--- app/components/product/productUpdate/productUpdate.js
+++ app/components/product/productUpdate/productUpdate.js
@@ -4,14 +4,15 @@ let Product = require('../../../../config/models/product');
 Product = Product(db.sequelize, db.Sequelize);
 
 const updateProduct = async(product_id, body) => {
-    const {productName, productHref, productCategory, productDescription, productPhoto, productPrice} = body;
+    const {productName, productHref, productCategory, productDescription, productPhoto, productPrice, productAmount} = body;
     const newDataProduct = {
         product_href: productHref,
         product_name: productName,
         product_category: productCategory,
         product_description: productDescription,
         product_photo: productPhoto,
-        product_price: productPrice
+        product_price: productPrice,
+        product_amount: productAmount
     }
     const [updatedRow] = await Product.update({...newDataProduct}, {where: {product_id}});
     return updatedRow;
