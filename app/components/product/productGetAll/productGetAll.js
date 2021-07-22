c1a001e68166d28a49342acb064ea00a084c612c
andresarangoo <arango.ortiz.felipe@accenture.com>
2021-07-22 08:49:35 -0500

    Update get all with product amount

--- app/components/product/productGetAll/productGetAll.js
+++ app/components/product/productGetAll/productGetAll.js
@@ -13,7 +13,8 @@ const getProducts = async() => {
             productCategory: product.product_category,
             productDescription: product.product_description,
             productPhoto: product.product_photo,
-            productPrice: product.product_price
+            productPrice: product.product_price,
+            productAmount: product.product_amount
         }
     });
     return productsFormatted;
