64f1e05640088d39880666de96db0966cc3d3faf
andresarangoo <arango.ortiz.felipe@accenture.com>
2021-07-22 08:38:57 -0500

    Update field size and add product amount

--- config/models/product.js
+++ config/models/product.js
@@ -4,24 +4,24 @@ module.exports = (sequelize, DataTypes) => {
     return sequelize.define('Product', {
 
         product_id:{
-            type: DataTypes.INTEGER(11),
+            type: DataTypes.INTEGER(9),
             allowNull: false,
             primaryKey: true,
             autoIncrement: true
         },
 
         product_href:{
-            type: DataTypes.STRING(250),
+            type: DataTypes.STRING(25),
             allowNull: false
         },
         
         product_name: {
-            type: DataTypes.STRING(250),
+            type: DataTypes.STRING(50),
             allowNull: false
         },
 
         product_category: {
-            type: DataTypes.STRING(250),
+            type: DataTypes.STRING(30),
             allowNull: false
         },
 
@@ -36,7 +36,11 @@ module.exports = (sequelize, DataTypes) => {
         }, 
 
         product_price: {
-            type: DataTypes.INTEGER(11),
+            type: DataTypes.INTEGER(9),
+            allowNull: false,
+        },
+        product_amount: {
+            type: DataTypes.INTEGER(9),
             allowNull: false,
         }
     },{
